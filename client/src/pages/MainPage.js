6f062171b02cb7839fcc2de6714d9a0c3ddde709
Randika-BLS <sithumrandika85@gmail.com>
2024-09-29 03:42:59 +0530

    config clilent with tailwindcss

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <div>
+      <h1>Convert Your Currencies Today</h1>
+      <p>
+        Welcome to "Convert Your Currencies Today"!. This Application allows you
+        to easily convert currencies based on the latest exchange rates. Whether
+        you're planning a trip, managing your finanaces, or simply curious about
+        the value of your money in different currencies, this tool is here to
+        help.
+      </p>
+    </div>
+  );
+}
+
+export default MainPage;
