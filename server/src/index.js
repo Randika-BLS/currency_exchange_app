3d56f013348b0d482afaf01aae5cc3c54af32353
Randika-BLS <sithumrandika85@gmail.com>
2024-09-29 21:00:20 +0530

    added

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,9 @@
-console.log("Hello World");
-console.log("express");
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+//middle users
+app.use(express.json());
+app.use(cors());
